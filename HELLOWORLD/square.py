40ffdce822adfae2f3184d2437ff5c5cf2117531
Joemugare <134427671+Joemugare@users.noreply.github.com>
2023-07-17 13:28:18 +0300

    Delete HELLOWORLD directory

--- HELLOWORLD/square.py
+++
@@ -1,8 +0,0 @@
-def get_square(number):
-    square = number ** 2
-    return square
-
-# Example usage
-input_number = float(input("Enter a number: "))
-result = get_square(input_number)
-print(f"The square of {input_number} is {result}.")
